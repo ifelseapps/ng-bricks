b24501561dd975c09b2e815da7dd21441877e66f
Maxim Pavlov <ifelseapps@gmail.com>
2021-01-01 02:15:35 +0300

    Combobox: popup base logic

--- projects/bricks/src/lib/bricks.module.ts
+++ projects/bricks/src/lib/bricks.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BricksComponent } from './bricks.component';
 import { ComboboxComponent } from './components/combobox/combobox.component';
-
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [BricksComponent, ComboboxComponent],
   imports: [
+    OverlayModule,
+    CommonModule
   ],
   exports: [BricksComponent, ComboboxComponent]
 })
--- projects/bricks/src/lib/components/combobox/combobox.component.ts
+++ projects/bricks/src/lib/components/combobox/combobox.component.ts
@@ -1,14 +1,45 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FocusMonitor } from '@angular/cdk/a11y';
+import { merge, Observable, Subscription } from 'rxjs';
+import { filter, mapTo } from 'rxjs/operators';
+import { CdkConnectedOverlay } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'lib-combobox',
   templateUrl: './combobox.component.html',
-  styleUrls: ['./combobox.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./combobox.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
-export class ComboboxComponent implements OnInit {
-  constructor() { }
+export class ComboboxComponent implements OnInit, OnDestroy {
+  constructor(private focusMonitor: FocusMonitor) { }
+
+  @ViewChild('label', { static: true })
+  labelRef: ElementRef<HTMLElement>;
+
+  @ViewChild(CdkConnectedOverlay, { static: true })
+  popupRef: CdkConnectedOverlay;
+
+  isVisiblePopup$: Observable<boolean>;
+
+  private masterSubscription = new Subscription();
 
   ngOnInit(): void {
+    const triggerFocus$ = this.focusMonitor.monitor(this.labelRef.nativeElement)
+      .pipe(
+        filter(origin => Boolean(origin)),
+        mapTo(true),
+      );
+
+    const backdropClick$ = this.popupRef.backdropClick
+      .pipe(mapTo(false));
+
+    backdropClick$.subscribe((x: any) => console.log(x));
+
+    this.isVisiblePopup$ = merge(triggerFocus$, backdropClick$);
+  }
+
+  ngOnDestroy(): void {
+    this.masterSubscription.unsubscribe();
   }
 }
