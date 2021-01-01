50bfca1f0a8523cf97c20514ad9f823840f98feb
Maxim Pavlov <ifelseapps@gmail.com>
2021-01-01 05:15:57 +0300

    Combobox: запретил выход фокуса при открытом popup'е

--- projects/bricks/src/lib/bricks.module.ts
+++ projects/bricks/src/lib/bricks.module.ts
@@ -3,13 +3,15 @@ import { BricksComponent } from './bricks.component';
 import { ComboboxComponent } from './components/combobox/combobox.component';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { CommonModule } from '@angular/common';
+import { A11yModule } from '@angular/cdk/a11y';
 
 
 @NgModule({
   declarations: [BricksComponent, ComboboxComponent],
   imports: [
     OverlayModule,
-    CommonModule
+    CommonModule,
+    A11yModule
   ],
   exports: [BricksComponent, ComboboxComponent]
 })
