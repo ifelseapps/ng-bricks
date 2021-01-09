2da134ad2316e52ac37773666e5683d05616f7fb
Maxim Pavlov <ifelseapps@gmail.com>
2021-01-09 19:20:09 +0300

    Оболочка: генерация api

---
+++ scripts/generate-docs.js
@@ -0,0 +1,23 @@
+const TypeDoc = require('typedoc');
+const typedocParser = require('@ifelseapps/typedoc-json-angular-parser');
+
+async function main() {
+  const app = new TypeDoc.Application();
+  app.options.addReader(new TypeDoc.TSConfigReader());
+  app.bootstrap({
+    // typedoc options here
+    entryPoints: ['./projects/bricks/src/lib/components/combobox/combobox.component.ts'],
+  });
+
+  const project = app.convert();
+
+  if (project) {
+    const outputDir = 'docs';
+    await app.generateJson(project, outputDir + '/documentation.json');
+
+    const json = require(`../${outputDir}/documentation.json`);
+    typedocParser.parse(json, { outputDir: '../../../src/assets/components-api' });
+  }
+}
+
+main().catch(console.error);
